d65856da219a79643cc3acbc0c73bf7a60a02a55
Puteulanus <mj19941230@gmail.com>
2015-02-17 15:36:34 +0800

    Update server.php

--- server.php
+++ server.php
@@ -2,7 +2,8 @@
 if (!(json_decode($_GET['servers'],true))){exit;}
 // 设定节点列表
 $node_list = array(
-    'Coding' => 'http://mcping-5f55e.coding.io/node.php');
+    'Coding' => 'http://mcping-5f55e.coding.io/node.php',
+    'OSC' => 'http://aya78820.oschina.mopaas.com/node.php');
 // 获取节点测速数据
 $servers_list = $_GET['servers'];
 $reply = array();
